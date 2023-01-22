a845e5fd11bbe7070cab71957de6d412769a0287
Charmy Chen <charmyminchen@gmail.com>
2023-01-23 00:15:55 +0400

    initial commit

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,5 +1,20 @@
 #Prompt
-oh-my-posh init pwsh --config 'C:\Users\97155\.config\powershell\min.omp.json' | Invoke-Expression
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'min.omp.json'
+oh-my-posh init pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+#Icons
+Import-Module Terminal-Icons
+
+#PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+#Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 #Alias
 Set-Alias vim nvim
@@ -8,3 +23,9 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+#Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
