2d9fe69a56314ea5e7f7ef50aff39f2919b29401
Charmy Chen <charmyminchen@gmail.com>
2023-01-22 22:57:23 +0400

    dotfiles update

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,10 @@
+#Prompt
+oh-my-posh init pwsh --config 'C:\Users\97155\.config\powershell\min.omp.json' | Invoke-Expression
+
+#Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
