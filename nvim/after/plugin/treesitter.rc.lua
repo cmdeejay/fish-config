c3615eaeb767a4a0171b22f0ab09ed451dd41435
Charmy Chen <charmyminchen@gmail.com>
2023-02-15 01:15:12 +0400

    nvim updated

---
+++ nvim/after/plugin/treesitter.rc.lua
@@ -0,0 +1,34 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ignore_install = {
+    "norg"
+  },
+  ensure_installed = {
+    "tsx",
+    "markdown",
+    "markdown_inline",
+    "toml",
+    "json",
+    "yaml",
+    "css",
+    "html",
+    "lua",
+    "python",
+  },
+  autotag = {
+    enable = true,
+  },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
